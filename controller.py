a1c088516f487fd013f7922680d568a7f85a3202
Trevor Kirby <trevor@Trevors-MacBook-Pro-4.local>
2023-03-15 09:59:10 -0700

    experiment route changed counts WIP

--- controller.py
+++ controller.py
@@ -1,4 +1,5 @@
 import os
+import random
 from lotus_configurator import Lotus_configurator
 from main import Interpreter
 from multiprocessing import Pool
@@ -13,7 +14,6 @@ def run_scenario(base_scenario, config: Lotus_configurator):
     interpreter.execute(execution_lines)
     interpreter.do_addAllASInit("")
     interpreter.do_run("")
-    
 
     attack = config.gen_attack()
     aspa_config = config.gen_aspa()
@@ -28,11 +28,30 @@ def run_scenario(base_scenario, config: Lotus_configurator):
 
     print(f"aspa_config: {aspa_config}\nattack: {attack}\n")
     updates = interpreter.run_updates
+    changes = 0
     for idx, val in enumerate(updates):
         old, new = val
         print(f"change {idx}\nold: {old}\nnew: {new}\n")
+        changes += 1
+    return changes
 
 if __name__ == "__main__":
-    p = Pool(2)
-    configs = [(BASE_SCENARIO, Lotus_configurator(0,0)), (BASE_SCENARIO, Lotus_configurator(1,0))]
-    p.starmap(run_scenario, configs)
+    all_asns = []
+    with open(BASE_SCENARIO, 'r') as infile:
+        data = infile.read()
+        for line in data.split('\n'):
+            if line.startswith('addAS '):
+                all_asns.append(int(line[6:]))
+    
+    with_aspa_changes = 0
+    without_aspa_changes = 0
+    seed = 0
+    for _ in range(5):
+        p = Pool(2)
+        configs = [(BASE_SCENARIO, Lotus_configurator(0,0,all_asns,seed)), (BASE_SCENARIO, Lotus_configurator(1,0,all_asns,seed))]
+        changes = p.starmap(run_scenario, configs)
+        with_aspa_changes += changes[0]
+        without_aspa_changes += changes[1]
+        seed += 1
+    print(f'Routes changed with ASPA: {with_aspa_changes}')
+    print(f'Routes changed without ASPA: {without_aspa_changes}')
--- lotus_configurator.py
+++ lotus_configurator.py
@@ -2,16 +2,18 @@ import random
 from main import Interpreter
 
 class Lotus_configurator:
-    def __init__(self, aspa, attack, seed=None):
+    def __init__(self, aspa, all_asns, seed=None):
         if seed:
             random.seed(seed)
 
+        self.all_asns = all_asns
         self.aspa_situation = aspa
         self.attack_type = attack
         
         self.attack_list = []
+        self.target = None
 
-    def gen_aspa(self) -> list[str]:
+    def gen_aspa(self, all_asns) -> list[str]:
         setASPV_str = "setASPV {} on {}"
         autoASPA_str = "autoASPA {} {}"
         match self.aspa_situation:
@@ -22,11 +24,13 @@ class Lotus_configurator:
         # return ["autoASPA 1 1", "setASPV 25 on 3", "setASPV 11 on 3"]
 
     def _international(self, attack_str:str):
+        attacker = random.choice(self.all_asns)
+        #self.target = rando
         return ["genAttack 46320 39875"]
         # return [attack_str.format()]
         pass
 
-    def gen_attack(self) -> list[str]:
+    def gen_attack(self, all_asns) -> list[str]:
         attack_str = "genAttack {} {}"
         match self.attack_type:
             case 0:
